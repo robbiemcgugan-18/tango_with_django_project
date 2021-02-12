8d582c903acb6d2c8db64d4e03eb170eafa33022
Robbie McGugan <2523558M@student.gla.ac.uk>
2021-02-12 18:04:44 +0000

    chapter 4 updated (removed test)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
+                'django.template.context_processors.media', 
             ],
         },
     },
@@ -110,8 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-LOGIN_URL = 'rango:login'
 
 
 # Internationalization
